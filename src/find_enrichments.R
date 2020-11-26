c95daaf3b35199725e41501e74523bdccd6f8a00
Sergej Andrejev <sandrejev@gmail.com>
2020-11-26 02:15:40 +0100

    Start developing enrichmennt pipeline. Fix find_offtargets.R to use Biostrings to get fasta

--- src/find_enrichments.R
+++ src/find_enrichments.R
@@ -5,18 +5,88 @@ library(stringr)
 bait_region_size = 4e6
 
 # breaksites_df = readr::read_tsv("data/breaksites_primers.tsv")
-offtargets_df = readr::read_tsv("data/offtargets.tsv") %>% # Doesn't have position for Chr6
+offtargets_df = readr::read_tsv("data/offtargets_predicted.tsv") %>% # Doesn't have position for Chr6
   dplyr::filter(offtarget_mismatches<5)
-table(offtargets_df$bait_chrom, offtargets_df$offtarget_mismatches)
 
 breaksites_df = offtargets_df %>% # Doesn't have position for Chr6
   dplyr::filter(offtarget_mismatches<5) %>%
   dplyr::select(bait_chrom, bait_position=offtarget_start)
 
-macs2_outdir = "data/macs2"
-macs2_cols = c("junction_chrom", "junction_start", "junction_end", "junction_name", "junction_score", "junction_strand")
-for(macs2_input in list.files("data/breaks", pattern="*.bed", full.names=T)) {
-    bait_chrom = gsub("Chr", "chr", (gsub("_APH_no10kb_Merge\\.bed$", "", basename(macs2_input))))
+tempdir = "tmp"
+enrichment_outdir = "data/enrichment"
+junctions_cols = c("junction_chrom", "junction_start", "junction_end", "junction_name", "junction_score", "junction_strand")
+peaks_cols = c("control_peak_chrom", "control_peak_start", "control_peak_end", "control_peak_length", "control_peak_summit", "control_peak_pileup", "control_peak_log10pvalue", "control_peak_fc", "control_peak_log10qvalue", "control_peak_name")
+peaks_coltypes = cols(
+  control_peak_chrom = col_character(),
+  control_peak_start = col_double(),
+  control_peak_end = col_double(),
+  control_peak_length = col_double(),
+  control_peak_summit = col_double(),
+  control_peak_pileup = col_double(),
+  control_peak_log10pvalue = col_double(),
+  control_peak_fc = col_double(),
+  control_peak_log10qvalue = col_double(),
+  control_peak_name = col_character()
+)
+
+results = data.frame()
+for(breaks_bed in list.files("data/breaks", pattern="*_APH_no10kb_Merge.bed", full.names=T)) {
+    bait_chrom = gsub("(chr)([^_]+).*$", "\\L\\1\\2", basename(breaks_bed), perl=T, ignore.case=T)
+    breaks_control_bed = gsub("_APH_no10kb_Merge.bed", "_DMSO.bed", breaks_bed)
+    breaks_filtered_bed = paste0("tmp/", basename(gsub("\\.bed$", "_filtered.bed", breaks_bed)))
+    writeLines(stringr::str_glue("Processing {bait}...", bait=bait_chrom))
+    if(!file.exists(breaks_control_bed)) {
+        writeLines("Control doesn't exist. Skipping...")
+        next()
+    }
+
+    output_control_enrichment = paste0(tempdir, "/", bait_chrom, "_control")
+
+    # Run enrichment detection on control
+    writeLines(stringr::str_glue("Calculate enrichments for control: '{output}'...", output=output_control_enrichment))
+    cmd_enrichment_control = stringr::str_glue(
+      "macs2 callpeak -t {input} -f BED -g mm --keep-dup all -n {output} --outdir {outdir} --nomodel --extsize 2000 -q 0.01 --llocal 10000000",
+      input=breaks_control_bed, outdir=dirname(output_control_enrichment), output=basename(output_control_enrichment))
+    system(cmd_enrichment_control)
+    control_enrichment_df = readr::read_tsv(paste0(output_control_enrichment, "_peaks.xls"), col_names=names(peaks_coltypes$cols), comment="#", skip=22, col_types=peaks_coltypes) %>%
+      dplyr::mutate(bait_chrom=bait_chrom)
+    results = dplyr::bind_rows(results, control_enrichment_df)
+    #
+    #
+    # macs2_input_df = readr::read_tsv(macs2_input, col_names=macs2_cols) %>%
+    #   dplyr::mutate(bait_chrom=bait_chrom) %>%
+    #   dplyr::inner_join(breaksites_df, by="bait_chrom") %>%
+    #   dplyr::filter(!dplyr::between(junction_start, bait_position - bait_region_size/2, bait_position + bait_region_size/2)) %>%
+    #   dplyr::select(dplyr::starts_with("junction_")) %>%
+    #   dplyr::distinct(junction_chrom, junction_start, .keep_all=T)
+    #
+    # readr::write_tsv(macs2_input_df[macs2_cols], file=macs2_input_filtered, col_names=F)
+    #
+    # macs2_cmd = stringr::str_glue(
+    #   "macs2 callpeak -t {input} -f BED -g mm --keep-dup all -n {output} --outdir {outdir} --nomodel --extsize 2000 -q 0.01 --llocal 10000000",
+    #   input=macs2_input_filtered, outdir=macs2_outdir, output=macs2_sample)
+    # system(macs2_cmd)
+    #
+    # readr::read_tsv(paste0(macs2_outdir, "/", macs2_sample, "_summits.bed"), col_names=F)
+}
+
+
+offtargets_ranges = with(offtargets_df, IRanges::IRanges(start=offtarget_start, end=offtarget_end))
+control_junctions_ranges = with(results,  IRanges::IRanges(start=control_peak_start, end=control_peak_end))
+validated_offtargets_overlapping = validated_offtargets %>%
+dplyr::inner_join(as.data.frame(IRanges::mergeByOverlaps(offtargets_ranges, control_junctions_ranges)), by=c("offtarget_start"="control_junctions_ranges.start", "offtarget_end"="validated_offtargets_ranges.end")) %>%
+dplyr::mutate(offtarget_validated=T) %>%
+dplyr::select(bait_name, bait_chrom, offtarget_is_primary, offtarget_chrom, offtarget_strand, offtarget_start, offtarget_end, offtarget_sequence, offtarget_start, offtarget_end, primer_alignment_start=primers_targets_ranges.start, primer_alignment_end=primers_targets_ranges.end, offtarget_validated)
+
+
+
+
+
+
+for(breaks_bed in list.files("data/breaks", pattern="*DMSO.bed", full.names=T)) {
+    bait_chrom = gsub("(chr)([^_]+).*$", "\\L\\1\\2", basename(breaks_bed), perl=T, ignore.case=T)
+
+
     macs2_input_filtered = paste0("tmp/", basename(gsub("\\.bed$", "_filtered.bed", macs2_input)))
 
     writeLines(paste0("Processing ", bait_chrom, "..."))
--- src/find_offtargets.R
+++ src/find_offtargets.R
@@ -12,53 +12,28 @@ library(ggplot2)
 library(Biostrings)
 library(BSgenome)
 
-Biostrings::readDNAStringSet("nm.fasta")
-subseq(s, start=c(1, 2, 3), end=c(3, 6, 5))
-
-getfasta = function(df, chrom_col, start_col, end_col, strand_col) {
-    df.plus = df[df[[strand_col]]=="+",]
-    files = c(input.plus=tempfile(), output.plus=tempfile(), input.minus=tempfile(), output.minus=tempfile())
-    writeLines(stringr::str_glue("{chr}:{start}-{end}", chr=df.plus[[chrom_col]], start=df.plus[[start_col]], end=df.plus[[end_col]]), con=files["input.plus"])
-    stringr::str_glue("samtools faidx data/mm9/mm9.fa -i -r {input} > {output}", input=files["input.plus"], output=files["output.plus"])
-
-    df.minus = df[df[[strand_col]]=="-",]
-
-    stringr::str_glue("{chr}:{start}-{end}", chr=df.minus[[chrom_col]], start=df.minus[[start_col]], end=df.minus[[end_col]])
-    writeLines(validated_offtargets.faidx %>% dplyr::filter(offtarget_strand=="+") %>% .$offtarget_faidx, con="tmp/offtargets_pos_plus.bed")
-
-
-}
 
 analyze.primers_in_mm9 = function() {
-  validated_offtargets = validated_offtargets %>%
+  genome_mm9 = Biostrings::readDNAStringSet("data/mm9/mm9.fa.gz", "fasta")
+  validated_offtargets = readr::read_tsv("data/offtargets_computational_wei.tsv") %>%
     dplyr::mutate(
-      offtarget_score=0,
-      offtarget_start=ifelse(offtarget_strand=="+", offtarget_start, offtarget_start-3-1),
-      offtarget_end=ifelse(offtarget_strand=="+", offtarget_end+3+1, offtarget_end)
-    ) %>%
-    dplyr::select(offtarget_chrom, offtarget_start, offtarget_end, offtarget_id, offtarget_score, offtarget_strand) %>%
-    dplyr::mutate(offtarget_sequence=as.character(Biostrings::getSeq(ref, with(validated_offtargets, GRanges(offtarget_chrom, IRanges(start=offtarget_start, end=offtarget_end), strand=offtarget_strand))))) %>%
+      offtarget_and_pam_start=ifelse(offtarget_strand=="+", offtarget_start, offtarget_start-3),
+      offtarget_and_pam_end=ifelse(offtarget_strand=="+", offtarget_end+3, offtarget_end)) %>%
+    # dplyr::select(offtarget_chrom, offtarget_start, offtarget_end, offtarget_id, offtarget_score, offtarget_strand) %>%
+    dplyr::mutate(offtarget_and_pam_sequence=as.character(Biostrings::getSeq(genome_mm9, GRanges(offtarget_chrom, IRanges(start=offtarget_and_pam_start, end=offtarget_and_pam_end), strand=offtarget_strand)))) %>%
     dplyr::mutate(
       offtarget_and_pam_sequence=toupper(offtarget_and_pam_sequence),
       offtarget_sequence2=substr(offtarget_and_pam_sequence, 1, nchar(offtarget_and_pam_sequence)-3),
-      offtarget_pam_sequence=substr(offtarget_and_pam_sequence, nchar(offtarget_and_pam_sequence)-3+1, nchar(offtarget_and_pam_sequence)),
+      offtarget_pam_sequence=substr(offtarget_and_pam_sequence, nchar(offtarget_and_pam_sequence)-2, nchar(offtarget_and_pam_sequence)),
       offtarget_has_pam=grepl("^[A-Z]GG$", toupper(offtarget_pam_sequence))
     ) %>%
-    dplyr::select(offtarget_id, offtarget_and_pam_sequence, offtarget_sequence2, offtarget_pam_sequence, offtarget_has_pam)
-
-
-  validated_offtargets = validated_offtargets %>%
-    dplyr::inner_join(validated_offtargets_sequences, by="offtarget_id") %>%
     dplyr::group_by(bait_chrom) %>%
     dplyr::mutate(offtarget_primary_sequence=offtarget_sequence[offtarget_is_primary==1]) %>%
     dplyr::ungroup()
+
   offtarget_sequences = strsplit(validated_offtargets$offtarget_sequence,"")
   offtarget_primary_sequences = strsplit(validated_offtargets$offtarget_primary_sequence,"")
   validated_offtargets$offtarget_mismatches_check = sapply(1:nrow(validated_offtargets), function(i) sum(offtarget_sequences[[i]]!=offtarget_primary_sequences[[i]]))
-
-
-  x = validated_offtargets %>% dplyr::filter(offtarget_has_pam & offtarget_sequence==offtarget_sequence2 & offtarget_mismatches_check==offtarget_mismatches)
-  table(x$bait_chrom, x$offtarget_mismatches)
 
   #
   # Find primers position in genome
@@ -88,20 +63,18 @@ analyze.primers_in_mm9 = function() {
     dir.create("data/mm9/bowtie1")
     system("bowtie-build --threads 30 data/mm9/mm9.fa.gz data/mm9/bowtie1/mm9")
   }
-  for(i in 0:3) {
-    system(stringr::str_glue("bowtie --threads 30 --tryhard --all -v {i} --seedlen 5 -f --sam --no-unal data/mm9/bowtie1/mm9 tmp/primers.fa > tmp/primers_bowtie{i}.sam", i=i))
-    system(stringr::str_glue("samtools sort -@ 30 tmp/primers_bowtie{i}.sam > tmp/primers_bowtie{i}.bam", i=i))
-  }
+  system("bowtie --threads 30 --tryhard --all -v 3 --seedlen 5 -f --sam --no-unal data/mm9/bowtie1/mm9 tmp/primers.fa > tmp/primers_bowtie3.sam")
+  system("samtools sort -@ 30 tmp/primers_bowti3.sam > tmp/primers_bowtie3.bam")
 
   # Analyze genome position and give final output
   primers_alignments_param = ScanBamParam(tag=c("AS", "NM"), what=c("qname", "rname", "strand", "flag", "pos", "qwidth",  "cigar", "mapq", "qual"))
   primers_alignments = data.frame()
-  for(bam_file in c("tmp/primers_bwa.bam", paste0("tmp/primers_bowtie", 0:3, ".bam"))) {
+  for(bam_file in c("tmp/primers_bwa.bam", "tmp/primers_bowtie3.bam")) {
     primers_alignments.d = lapply(Rsamtools::scanBam(bam_file, param=primers_alignments_param), function(z) {
       d = data.frame(z[names(z)!="tag"])
       d$primer_alignment_mismatches=z$tag$NM
       cbind(d, do.call(rbind, lapply(d$flag, SamSeq::samFlags))) })[[1]] %>%
-      dplyr::mutate(primer_sequence_name=qname, primer_alignment_end=pos+qwidth) %>%
+      dplyr::mutate(primer_sequence_name=qname, primer_alignment_end=pos+qwidth-1) %>%
       dplyr::mutate(primer_alignment_primary=!NOT_PRIMARY_ALIGNMENT) %>%
       dplyr::filter(!READ_UNMAPPED) %>%
       dplyr::select(primer_sequence_name, primer_alignment_chrom=rname, primer_alignment_strand=strand, primer_alignment_start=pos, primer_alignment_end, primer_alignment_flag=flag, primer_alignment_cigar=cigar, primer_alignment_len=qwidth, primer_alignment_mismatches, primer_alignment_primary)
@@ -109,35 +82,16 @@ analyze.primers_in_mm9 = function() {
   }
   primers_alignments = primers_alignments %>%
     dplyr::distinct(primer_sequence_name, primer_alignment_chrom, primer_alignment_strand, primer_alignment_start, primer_alignment_end, .keep_all=T) %>%
-    dplyr::mutate(primer_alignment_id=paste0(primer_sequence_name, "_", 1:n())) %>%
-    dplyr::filter(primer_alignment_id %in% c("Chr4_51Mb_plus_sgRNA_313442", "Chr6_70Mb_plus_sgRNA_3321"))
-
-  # Extract sequences that were aligned to primers
-  primers_alignments %>%
     dplyr::mutate(
-      score=0,
-      primer_alignment_start=ifelse(primer_alignment_strand=="+", primer_alignment_start-1, primer_alignment_start-3-1),
-      primer_alignment_end=ifelse(primer_alignment_strand=="+", primer_alignment_end+3-1, primer_alignment_end-1)) %>%
-    dplyr::select(primer_alignment_chrom, primer_alignment_start, primer_alignment_end, primer_alignment_id, score, primer_alignment_strand) %>%
-    readr::write_tsv(file=paste0("tmp/primers_alignments_pos.bed"), col_names=F)
-  system("bedtools getfasta -fi data/mm9/mm9.fa -bed tmp/primers_alignments_pos.bed -bedOut -s > tmp/primers_alignments_seq.bed")
-
-  #x = primers_alignments %>%
-  #  dplyr::filter(primer_alignment_id %in% c("Chr4_51Mb_plus_sgRNA_313442", "Chr6_70Mb_plus_sgRNA_3321"))
-  #x$primer_alignment_end - x$primer_alignment_start
-
-  primers_alignments_sequences = readr::read_tsv("tmp/primers_alignments_seq.bed", col_names=c("primer_alignment_chrom", "primer_alignment_start", "primer_alignment_end", "primer_alignment_id", "primer_alignment_score", "primer_alignment_strand", "primer_alignment_and_pam_sequence")) %>%
-    dplyr::rowwise() %>%
-    dplyr::mutate(
+      primer_alignment_and_pam_start=ifelse(primer_alignment_strand=="+", primer_alignment_start, primer_alignment_start-3),
+      primer_alignment_and_pam_end=ifelse(primer_alignment_strand=="+", primer_alignment_end+3, primer_alignment_end),
+      primer_alignment_and_pam_sequence=as.character(Biostrings::getSeq(genome_mm9, GRanges(primer_alignment_chrom, IRanges(start=primer_alignment_and_pam_start, end=primer_alignment_and_pam_end), strand=primer_alignment_strand))),
       primer_alignment_sequence=substr(primer_alignment_and_pam_sequence, 1, nchar(primer_alignment_and_pam_sequence)-3),
       primer_alignment_pam_sequence=substr(primer_alignment_and_pam_sequence, nchar(primer_alignment_and_pam_sequence)-3+1, nchar(primer_alignment_and_pam_sequence)),
       primer_alignment_N_total=nchar(gsub("[^N]", "", primer_alignment_sequence)),
       primer_alignment_N_max=max(nchar(stringr::str_split(primer_alignment_sequence, "[^N]")[[1]])),
       primer_alignment_has_pam=grepl("^[A-Z]GG$", toupper(primer_alignment_pam_sequence))
-    ) %>%
-    dplyr::filter(primer_alignment_N_max<3) %>%
-    dplyr::select(primer_alignment_id, primer_alignment_and_pam_sequence, primer_alignment_sequence, primer_alignment_pam_sequence, primer_alignment_N_total, primer_alignment_N_max, primer_alignment_has_pam)
-
+    )
 
   validated_offtargets_ranges = with(validated_offtargets, IRanges::IRanges(start=offtarget_start, end=offtarget_end))
   primers_targets_ranges = with(primers_alignments,  IRanges::IRanges(start=primer_alignment_start, end=primer_alignment_end))
@@ -148,59 +102,41 @@ analyze.primers_in_mm9 = function() {
 
   primers_targets = primers %>%
     dplyr::inner_join(primers_alignments, by="primer_sequence_name") %>%
-    dplyr::inner_join(primers_alignments_sequences, by="primer_alignment_id") %>%
-    dplyr::filter(primer_alignment_len==primer_len) %>%
     dplyr::left_join(validated_offtargets_overlapping, by=c("bait_name", "bait_chrom", "primer_alignment_strand"="offtarget_strand", "primer_alignment_chrom"="offtarget_chrom", "primer_alignment_start", "primer_alignment_end")) %>%
+    dplyr::filter(primer_alignment_len==primer_len) %>%
     dplyr::mutate(offtarget_validated=tidyr::replace_na(offtarget_validated, F)) %>%
     # dplyr::select(-primer_sequence_name, -primer_alignment_id) %>%
-    dplyr::mutate(primer_alignment_mismatches_rate=primer_alignment_mismatches/primer_len)
-
-  x = strsplit(toupper(primers_targets$primer_sequence),"")
-  y = strsplit(toupper(primers_targets$primer_alignment_sequence),"")
-  primers_targets$primer_alignment_mismatches_check = sapply(1:nrow(primers_targets), function(i) sum(x[[i]]!=y[[i]]))
-  primers_targets$primer_alignment_mismatches = primers_targets$primer_alignment_mismatches_check
-
-  primers_targets %>%
-    dplyr::mutate(primer_alignment_sequence_len=nchar(primer_alignment_sequence)) %>%
-    dplyr::filter(primer_alignment_mismatches < 8 & primer_alignment_sequence_len!=20) %>%
-    dplyr::group_by(primer_alignment_sequence_len) %>%
-    dplyr::slice(1) %>%
-    dplyr::ungroup() %>%
-    View()
-  table(nchar(primers_targets$primer_alignment_sequence), nchar(primers_targets$primer_sequence), primers_targets$primer_alignment_strand)
-
-  #primers_targets = primers_targets %>% dplyr::filter(primer_alignment_mismatches_check==primer_alignment_mismatches)
-  primers_targets = primers_targets %>%
-    dplyr::filter(primer_alignment_mismatches_check<=10 & primer_alignment_has_pam & !grepl("random|chrY", primer_alignment_chrom)) %>%
+    dplyr::mutate(primer_alignment_mismatches_rate=primer_alignment_mismatches/primer_len) %>%
+    dplyr::filter(primer_alignment_mismatches<=10 & primer_alignment_has_pam & !grepl("_|chrY", primer_alignment_chrom)) %>%
     dplyr::filter(primer_name=="sgRNA" & primer_alignment_has_pam & !grepl("random|chrY", primer_alignment_chrom))
+
   primers_targets %>%
     dplyr::mutate(offtarget_validated=ifelse(offtarget_validated, 1, 0)) %>%
     dplyr::select(bait_name, bait_chrom, offtarget_chrom=primer_alignment_chrom, offtarget_strand=primer_alignment_strand, offtarget_start=primer_alignment_start, offtarget_end=primer_alignment_end, offtarget_mismatches=primer_alignment_mismatches, offtarget_primer_sequence=primer_sequence, offtarget_sequence=primer_alignment_sequence) %>%
     readr::write_tsv(file="data/offtargets_predicted.tsv")
 
 
-
-
   pdf("reports/primers_offtargets.pdf", width=10, height=6)
-  ggplot(primers_targets) +
-    geom_histogram(aes(primer_alignment_mismatches, fill=primer_name), bins=10) +
-    labs(x="Error rate") +
-    theme_bw()
+
 
   primers_targets.ggplot = primers_targets %>%
     dplyr::mutate(primer_alignment_chrom=factor(primer_alignment_chrom)) %>%
     dplyr::filter(primer_alignment_mismatches<=5)
-
-
-  ggplot(primers_targets.ggplot) +
-    ggridges::geom_density_ridges(aes(x=primer_alignment_start/1e6, y=primer_alignment_chrom, height=..count.., color=primer_alignment_mismatches==0), stat = "binline", bins=1000, scale=1.5, draw_baseline=F) +
-    facet_wrap(~bait_chrom, ncol=3)
 
   primers_targets.ggplot_sum = primers_targets.ggplot %>%
     dplyr::group_by(bait_name, primer_alignment_mismatches) %>%
     dplyr::summarise(mismatches=n()) %>%
     reshape2::dcast(bait_name ~ primer_alignment_mismatches, value.var="mismatches") %>%
     replace(is.na(.), 0)
+
+  ggplot(primers_targets) +
+    geom_histogram(aes(primer_alignment_mismatches, fill=primer_name), bins=10) +
+    labs(x="Error rate") +
+    theme_bw()
+
+  ggplot(primers_targets.ggplot) +
+      ggridges::geom_density_ridges(aes(x=primer_alignment_start/1e6, y=primer_alignment_chrom, height=..count.., color=primer_alignment_mismatches==0), stat = "binline", bins=1000, scale=1.5, draw_baseline=F) +
+      facet_wrap(~bait_chrom, ncol=3)
 
   ggplot(primers_targets.ggplot) +
     geom_segment(aes(x=primer_alignment_start/1e6, xend=primer_alignment_start/1e6, y=as.numeric(primer_alignment_chrom)-0.2*(1-primer_alignment_mismatches_rate), yend=as.numeric(primer_alignment_chrom)+0.2*(1-primer_alignment_mismatches_rate), color=bait_chrom, size=(1-primer_alignment_mismatches_rate)*0.5)) +
